5d188b23aaa56688135c62d01a5ccc8508c33bba
joelmadadha <joelmadadha@gmail.com>
2026-07-10 18:58:16 -0300

    started to implement weather dataset. Might be replaced for another

---
+++ weather_data_clean.py
@@ -0,0 +1,9 @@
+import pandas as pd
+
+wdata = pd.read_csv(r"C:\Users\joel regular\Downloads\Git\HRMTransitWebsite-1\hfx_weather_data.csv", encoding = "cp1252")
+
+#Dropping columns that have few unique values as they will have no purpose
+wdata = wdata.drop(columns = [col for col in wdata.columns.tolist() if wdata[col].nunique()<2])
+
+
+print(wdata.head())
